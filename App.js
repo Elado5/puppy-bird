fc728466847554808d4892ba676c827ac0a4b869
Elad Or <elad725@gmail.com>
2020-12-24 01:36:30 +0200

    Working with expo, started creating game logic

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
+import Bird from './components/Bird'
 
 export default function App() {
+
+  const screenWidth = Dimensions.get("screen").width //ייבוא הרוחב של המסך בשימוש
+  const screenHeight = Dimensions.get("screen").height
+  const birdLeft = screenWidth / 2 //תחתית שמאל של הדיב
+  const [birdBottom, setBirdBottom] = useState(screenHeight/2) 
+  const [obstaclesLeft, setObstaclesLeft] = useEffect(screenWidth) 
+  const gravity = 3
+  let gameTimer;
+  let obstaclesLeftTimer;
+  //#region start bird falling
+  useEffect(() => {
+    if(birdBottom > 0) {
+      setInterval(() => {
+        setBirdBottom(birdBottom => birdBottom - gravity) //כל עוד הציפור לא נגעה בתחתית אני רוצה להוריד את הגובה שלה כל פרק זמן
+      }, 30)
+    }
+    return () => {
+      clearInterval(gameTimer) //אם התנאי לא מתממש - כלומר הציפור נגעה בתחתית
+    }
+  }, [birdBottom]) //the input
+  //#endregion
+
+ //#region start first obstacles
+  useEffect(() => {
+    if (obstaclesLeft > 0){
+      obstaclesLeftTimer = setInterval(() => {
+       setObstaclesLeft(obstaclesLeft => obstaclesLeft - 5) // יזיז את המכשולים שמאלה בפעימות של 5 פיקסלים
+      }, 30);
+    }
+    return () => {
+      clearInterval(obstaclesLeftTimer)
+    }
+  }, [obstaclesLeft])
+ //#endregion
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Bird 
+      birdBottom={birdBottom}
+      birdLeft={birdLeft}
+      />
     </View>
   );
 }
